558cb2976deefd120907bc1a7ee1f0fea83e66d1
Sade Phillips <89299922+CodeHousePro-lab@users.noreply.github.com>
2021-08-25 15:17:23 +0100

    Add files via upload

--- adventure_game1.py
+++ adventure_game1.py
@@ -12,20 +12,21 @@ def print_pause(message_to_print):
     print(message_to_print)
     time.sleep(2)
 
+
 def valid_input(prompt, options):
     while True:
         option = input(prompt).lower()
         if option in options:
             return option
-        print_pause(f'Sorry, the option "{options}"is invalid. Try Again!')     
-
+        print_pause(f'Sorry, the option "{options}"is invalid. Try Again!')  
 
 
 def play_again():
     choice = valid_input("Play again? [y|n]", ['y', 'n'])
     if choice == 'n':
         print('Thanks for playing! Goodbye!')
-        exit(0)        
+        exit(0)
+
 
 def intro():
     print_pause("You find yourself standing in an open field, "
@@ -36,15 +37,17 @@ def intro():
     print_pause("In front of you is a house "
                 "To the right of you is a "
                 "dark and spooky cave ")
+
+
 def play_game():
     intro()
     print_pause("Enter 1 to knock on the door of the house.")
     print_pause("Enter 2 to to peer into the cave.")
     print_pause("What would you like to do?")
     choice = valid_input('door or cave? (1|2)', ['1', '2'])
-    if choice == '1' :
+    if choice == '1':
         knock_on_door()
-    elif choice == '2' :
+    elif choice == '2':
         peer_into_cave()    
 
 def knock_on_door():
@@ -55,10 +58,11 @@ def knock_on_door():
     fight() 
 
 def fight():
-    choice = valid_input('Fight, run or give up? (1fight|2run|3exit)', ['1', '2', '3'])
+    choice = valid_input('1-Fight, 2-run or 3-give up? (1|2|3)', ['1', '2', '3'])
     if choice == '1':
         if "sword" in items:
-            print_pause("You take your fight stance! In your hand is the might 'sword of Thorvath!'")  
+            print_pause("You take your fighting stance! "
+                        "In your hand is the might 'sword of Thorvath!'")  
             print_pause("An evil "+selected_monster+" with "
                         "stringy white hair and a goti appears "
                         "The wicked "+selected_monster+" ATTACKS YOU! ")
@@ -72,9 +76,7 @@ def fight():
             print_pause("You do your best... "
                         "but your dagger is no match "
                         "for the "+selected_monster+".")
-            print_pause("You have been DEFEATED!")
-            
-        
+            print_pause("You have been DEFEATED!")                    
     if choice == '2':
         print_pause("(2) Run Away")
         print_pause("You retreat back into the field. "
@@ -82,7 +84,8 @@ def fight():
     if choice == '3':
         print_pause("Okay, Goodbye!")
         exit()                   
-    
+
+
 def peer_into_cave():
     print_pause("You step back and see a dark, "
                 "damp cave off into the distance")
@@ -106,13 +109,12 @@ def peer_into_cave():
 
 
 def game():
-
-    
     while True:
 
-        
-        play_game()
+        global selected_monster
+        selected_monster = random.choice(list(all_monsters))
 
+        play_game()
    
         play_again()
 
